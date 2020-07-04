09f24ce3c57c3267c479089e37c1610eea08a00e
imtaplet <rockgajoen@gmail.com>
2020-07-04 13:55:02 +0900

    add support for columns

--- src/SortableTable.elm
+++ src/SortableTable.elm
@@ -1,12 +1,13 @@
 module SortableTable exposing (Model, Msg, init, update, view)
 
-import Html exposing (Html, table, tbody, td, text, thead, tr)
+import Html exposing (Html, table, tbody, td, text, th, thead, tr)
 import Html.Events exposing (onClick)
 
 
 type alias Model =
     { sortColumn : Maybe String
     , reversed : Bool
+    , columns : List String
     }
 
 
@@ -16,7 +17,7 @@ type Msg
 
 init : Model
 init =
-    Model Nothing False
+    Model Nothing False [ "書籍名" ]
 
 
 update : Msg -> Model -> Model
@@ -43,8 +44,22 @@ view model items =
     table
         [ onClick (ClickColumn Nothing)
         ]
-        [ tbody []
-            (List.map viewRow reversedItems)
+        [ thead [] (List.map viewColumn model.columns)
+        , tbody [] (List.map viewRow reversedItems)
+        ]
+
+
+viewColumn : String -> Html Msg
+viewColumn item =
+    tr []
+        [ viewHeader item
+        ]
+
+
+viewHeader : String -> Html Msg
+viewHeader item =
+    th []
+        [ text item
         ]
 
 
